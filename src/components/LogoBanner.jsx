20765336bc61cd6392ef184bae153f5dde90e8b7
Suryakanta Prusty <kantaprustys@gmail.com>
2024-12-10 23:31:06 +0530

    changed the path of logos in LogoBanner component

--- src/components/LogoBanner.jsx
+++ src/components/LogoBanner.jsx
@@ -5,37 +5,37 @@ const LogoBanner = () => {
       <div className=" hidden lg:flex md:flex-row flex-col md:justify-center items-center space-x-12">
         {/* Logo Images */}
         <img
-          src="/public/images/logo-01.svg"
+          src="/images/logo-01.svg"
           alt="Logo 1"
           className="h-8 object-contain"
         />
         <img
-          src="/public/images/logo-02.svg"
+          src="/images/logo-02.svg"
           alt="Logo 2"
           className="h-14 object-contain"
         />
         <img
-          src="/public/images/logo-03.svg"
+          src="/images/logo-03.svg"
           alt="Logo 3"
           className="h-10 object-contain"
         />
         <img
-          src="/public/images/logo-04.svg"
+          src="/images/logo-04.svg"
           alt="Logo 4"
           className="h-10 object-contain"
         />
         <img
-          src="/public/images/logo-05.svg"
+          src="/images/logo-05.svg"
           alt="Logo 5"
           className="h-12 object-contain hidden md:block"
         />
         <img
-          src="/public/images/logo-06.svg"
+          src="/images/logo-06.svg"
           alt="Logo 6"
           className="h-12 object-contain hidden md:block"
         />
         <img
-          src="/public/images/logo-07.svg"
+          src="/images/logo-07.svg"
           alt="Logo 7"
           className="h-12 object-contain hidden md:block"
         />
@@ -44,24 +44,24 @@ const LogoBanner = () => {
         {/* Logo Images */}
         <div className="grid grid-cols-2 justify-center">
           <img
-            src="/public/images/logo-01.svg"
+            src="/images/logo-01.svg"
             alt="Logo 1"
             className="h-6 object-contain"
           />
           <img
-            src="/public/images/logo-02.svg"
+            src="/images/logo-02.svg"
             alt="Logo 2"
             className="h-9 object-contain my-auto"
           />
         </div>
         <div className="grid grid-cols-2">
           <img
-            src="/public/images/logo-03.svg"
+            src="/images/logo-03.svg"
             alt="Logo 3"
             className="h-8 object-contain"
           />
           <img
-            src="/public/images/logo-04.svg"
+            src="/images/logo-04.svg"
             alt="Logo 4"
             className="h-8 object-contain"
           />
